839b8f986049295e6a57778ebe0a61fdab130ec7
Greg Meiste <greg.meiste@gmail.com>
2014-12-01 20:09:26 -0600

    app: Support receiving GCM messages
    Signed-off-by: Greg Meiste <greg.meiste@gmail.com>

---
+++ app/src/main/java/com/meiste/tempalarm/gcm/GcmIntentService.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2014 Gregory S. Meiste  <http://gregmeiste.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.meiste.tempalarm.gcm;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.meiste.tempalarm.Log;
+
+public class GcmIntentService extends IntentService {
+
+    private static final String TAG = GcmIntentService.class.getSimpleName();
+
+    public GcmIntentService() {
+        super(GcmIntentService.class.getSimpleName());
+    }
+
+    @Override
+    protected void onHandleIntent(final Intent intent) {
+        final Bundle extras = intent.getExtras();
+        final GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+        final String messageType = gcm.getMessageType(intent);
+
+        if (extras != null && !extras.isEmpty()) {
+            if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+                Log.d(TAG, "GCM RECEIVED: " + extras.getString("message"));
+            }
+        }
+
+        GcmBroadcastReceiver.completeWakefulIntent(intent);
+    }
+}
