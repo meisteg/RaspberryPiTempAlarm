d3a70e7605e2cde85e52edbcc574ae12fedd5922
Greg Meiste <greg.meiste@gmail.com>
2015-12-29 11:11:49 -0600

    app: Rename RasPiApplication to TempAlarmApplication
    Continuing the quest to remove Raspberry Pi references.
    
    Signed-off-by: Greg Meiste <greg.meiste@gmail.com>

--- app/src/main/java/com/meiste/tempalarm/TempAlarmApplication.java
+++ app/src/main/java/com/meiste/tempalarm/TempAlarmApplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Gregory S. Meiste  <http://gregmeiste.com>
+ * Copyright (C) 2014-2015 Gregory S. Meiste  <http://gregmeiste.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 
-public class RasPiApplication extends Application {
+public class TempAlarmApplication extends Application {
 
     @Override
     public void onCreate() {
